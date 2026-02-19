3e80264c98f976c5e376a8e091ed34c4dfe87665
csavkomykola-cell <c.savko.mykola@student.uzhnu.edu.ua>
2026-02-19 21:17:22 +0200

    Add files via upload

---
+++ Lab_7/Lab_7_2/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+class BankTerminal
+{
+    public Action<int> OnMoneyWithdraw;
+    public void Withdraw(int amount)
+    {
+        Console.WriteLine($"Зняття {amount} грн...");
+        OnMoneyWithdraw?.Invoke(amount);
+    }
+}
+class Program
+{
+    static void Main()
+    {
+        BankTerminal terminal = new BankTerminal();
+        terminal.OnMoneyWithdraw += amount =>
+        {
+            Console.WriteLine($"Лог: знято {amount} грн");
+        };
+        terminal.Withdraw(100);
+        terminal.OnMoneyWithdraw = null;
+        terminal.Withdraw(200);
+        terminal.OnMoneyWithdraw?.Invoke(1000000);
+    }
+}
