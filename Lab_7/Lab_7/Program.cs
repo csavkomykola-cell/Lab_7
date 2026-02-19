2187ae6ebb726ac1658a297f9ed5a290803cd35e
csavkomykola-cell <c.savko.mykola@student.uzhnu.edu.ua>
2026-02-19 21:00:02 +0200

    Add files via upload

--- Lab_7/Lab_7/Program.cs
+++ Lab_7/Lab_7/Program.cs
@@ -6,9 +6,10 @@ class Program
     static void Main()
     {
         List<Action> actions = new List<Action>();
-        for (int i = 0; i < 5; i++)
+        for (int i = 1; i <= 5; i++)
         {
-            actions.Add(() => Console.WriteLine(i));
+            int copy = i;
+            actions.Add(() => Console.WriteLine(copy));
         }
         foreach (var action in actions)
         {
