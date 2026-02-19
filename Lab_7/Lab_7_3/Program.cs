d3990c70f2cff555c95770625b35b7bd600b6fcc
csavkomykola-cell <c.savko.mykola@student.uzhnu.edu.ua>
2026-02-19 21:45:23 +0200

    Add files via upload

---
+++ Lab_7/Lab_7_3/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Func<double, double> discountCalculator = null;
+        discountCalculator += price => price * 0.95;
+        discountCalculator += price => price * 0.90;
+        discountCalculator += price => price - 100;
+        double result = discountCalculator(1000);
+        Console.WriteLine($"Результат: {result}");
+    }
+}
